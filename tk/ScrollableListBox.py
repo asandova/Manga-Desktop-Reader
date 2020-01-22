a10028b4ce5dc12683f3c96bcb1b6fb31bef91a4
August Sandoval <august.sandoval96@gmail.com>
2020-01-22 01:00:02 -0700

    Added configurability to ScrollableListBox. Allow for scroll bars to be placed in different positions

--- tk/ScrollableListBox.py
+++ tk/ScrollableListBox.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 try:
-    from Tkinter import *
-    from Tkinter.ttk import *
+    from Tkinter import Tk, Frame, Scrollbar, Listbox ,LEFT, RIGHT, TOP, BOTTOM, BOTH, N, E, S ,W
+    from Tkinter.ttk import Style
 except ImportError:
-    from tkinter import *
-    from tkinter.ttk import *
+    from tkinter import Tk, Frame, Scrollbar, Listbox ,LEFT, RIGHT, TOP, BOTTOM, BOTH, N, E, S ,W
+    from tkinter.ttk import Style
 
 
 class ScrollableListbox(Frame):
@@ -36,25 +36,27 @@ class ScrollableListbox(Frame):
             self.grid_rowconfigure(0, weight=1)
             self.grid_rowconfigure(1,weight=0)
             self.__listpos[0] = 0
+            self.__HScrollPos[0] = 1
+            self.__VScrollPos[0] = 0
             
         elif hscrollside == "top":
             self.grid_rowconfigure(0, weight=0)
             self.grid_rowconfigure(1,weight=1)
             self.__listpos[0] = 1
-
+            self.__HScrollPos[0] = 0
+            self.__VScrollPos[0] = 1
 
         self.__VScroll.grid(row=self.__VScrollPos[0],column=self.__VScrollPos[1],sticky=N+S)    
         self.__HScroll.grid(row=self.__HScrollPos[0],column=self.__HScrollPos[1],sticky=E+W)
         self.__ListBox.grid(row=self.__listpos[0],column=self.__listpos[1],sticky=E+W+S+N)
 
-
-
     def add_entry(self):
         pass
     def remove_entry(self):
         pass
+
 if __name__ == "__main__":
     main = Tk()
-    test = ScrollableListbox(master=main)
+    test = ScrollableListbox(master=main,hscrollside="top")
     test.pack(side=LEFT, expand=1,fill=BOTH)
     main.mainloop()
